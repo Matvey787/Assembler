af9db866c1c35c48252cc7c0854e9babc8e8641b
Matvey787 <matveyklg@gmail.com>
2025-03-16 20:19:07 +0300

    myPrintf

--- ASM/NASM/64bit/test.c
+++ ASM/NASM/64bit/test.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
-
 extern void myprintf(const char *format, ...);
 
 int main()
 {
-    myprintf("%d", 13);
-    printf("\n");
+    myprintf("\n %o %b %c %d %d %d", 47, 11, 'Q', 4124, 423, 189);
 }
