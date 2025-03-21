573001905360436ff736f64cd4a6f315ca605ca3
Matvey787 <matveyklg@gmail.com>
2025-03-21 08:57:36 +0300

    c

--- ASM/NASM/64bit/test.c
+++ ASM/NASM/64bit/test.c
@@ -2,7 +2,7 @@ extern int myprintf(const char *format, ...);
 
 int main()
 {
-    if (myprintf("%s %5 %b\n","qwety", 8, 11, 18) == -1)
+    if (myprintf("%x\n", 17) == -1)
     {
         myprintf("%s\n", "Error!");
     };
